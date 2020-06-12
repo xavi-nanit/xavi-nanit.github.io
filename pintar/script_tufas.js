151aea9d92d5386f7cbb45e28b71c495425330ca
xavi-nanit <65017448+xavi-nanit@users.noreply.github.com>
2020-06-12 15:03:29 +0200

    Update script_tufas.js

--- pintar/script_tufas.js
+++ pintar/script_tufas.js
@@ -111,7 +111,7 @@
   
 }( jQuery ));
 
-$('#ActivityDIV'   ).makeSVGcolor('http://calaflora.com/nanit/pintar/images/tufas.svg')
+$('#ActivityDIV'   ).makeSVGcolor('https://xavi-nanit.github.io/pintar/images/tufas.svg')
 $('#btnRandom'     ).btnRandom()
 $('#btnClear'      ).btnClear()
 $('#btnDownloadSVG').btnDownload()
